c8da073b88fe829c015dc4c719f5690e8eab87e0
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-12-05 01:20:28 +0300

    refactor: :recycle: load only changelog.md

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -2,30 +2,16 @@ import React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
 
-let ChangelogPage = ({
-  data: {
-    changelog: {
-      edges: [
-        {
-          node: { html },
-        },
-      ],
-    },
-  },
-}) => (
+let ChangelogPage = ({ data: { changelog } }) => (
   <Layout>
-    <div dangerouslySetInnerHTML={{ __html: html }} />
+    <div dangerouslySetInnerHTML={{ __html: changelog.html }} />
   </Layout>
 )
 
 export let query = graphql`
-  query Changelog {
-    changelog: allMarkdownRemark {
-      edges {
-        node {
-          html
-        }
-      }
+  {
+    changelog: markdownRemark(fileAbsolutePath: { regex: "/CHANGELOG.md/" }) {
+      html
     }
   }
 `
