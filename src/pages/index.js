1b291f5a7e1f40b5dc6676ff5455a9e39fb5c0d4
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-12-15 17:53:53 +0300

    feat: :sparkles: pull about page from contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,24 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-const IndexPage = () => (
+const IndexPage = ({ data: { about } }) => (
   <Layout>
-    <h2>Summary</h2>
-
-    <p>
-      I have 5 years of experience as a software engineer. During this time I
-      had a chance to take part in many aspects of enterprise software
-      development:
-    </p>
-
-    <ul>
-      <li>
-        I was a lead/key developer in several big (50+ members, mostly teams
-        were distributed by different locations) projects: I communicated with a
-        customer, participated in tasks estimation, chose technology stack,
-        designed architecture solutions, wrote code, led team of 4-6 members,
-        conducted code reviews...
-      </li>
-
-      <li>
-        For 2 years I've been a trainer of front-end lab, also I've taken part
-        in several educational programs (react, angular 2) as a mentor / lector
-      </li>
-
-      <li>
-        I conduct technical interviews with people applying for front-end
-        positions in different locations (Russia, China, Mexico, Romania,
-        England...) and participate as an expert in assessments
-      </li>
-
-      <li>
-        I had several public talks both in local front-end community and company
-        open events (IT days, IT week...)
-      </li>
-    </ul>
-
-    <p>
-      All these activities gave me good technical background, understanding of
-      software development processes and communication skills: I worked with
-      JavaScript (ES5/ES6), TypeScript, CSS/HTML... frameworks and libraries
-      like angular, react, redux/flux..., nodejs (express, koa...), build tools
-      - webpack...; I know what CI/CD is, used different branching strategies,
-      worked with a few development methodologies (prefer scrum), understand how
-      testing is important and what kind of testing we can implement.
-    </p>
+    <div
+      dangerouslySetInnerHTML={{ __html: about.childContentfulRichText.html }}
+    />
   </Layout>
 )
+
+export let query = graphql`
+  query IndexQuery {
+    about: contentfulAboutContentRichTextNode {
+      childContentfulRichText {
+        html
+      }
+    }
+  }
+`
 
 export default IndexPage
