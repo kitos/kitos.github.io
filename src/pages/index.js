fd4d1f80cf101a8a813ecfe06a8f5415b2974a32
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-12-16 01:47:48 +0300

    feat: :sparkles: :lipstick: resolve #3 - add shields to about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import { Box, Flex } from '@rebass/grid'
 
 import Layout from '../components/layout'
-import { graphql } from 'gatsby'
+import { Shield } from '../components/shield'
 
 const IndexPage = ({ data: { about } }) => (
   <Layout>
+    <Flex mt="30px" flexWrap="wrap">
+      {about.shields.map(s => {
+        let [label, value] = s.split('|')
+
+        return (
+          <Box mr="10px">
+            <Shield {...{ label, value }} />
+          </Box>
+        )
+      })}
+    </Flex>
+
     <div
-      dangerouslySetInnerHTML={{ __html: about.childContentfulRichText.html }}
+      dangerouslySetInnerHTML={{
+        __html:
+          about.childContentfulAboutContentRichTextNode.childContentfulRichText
+            .html,
+      }}
     />
   </Layout>
 )
 
 export let query = graphql`
   query IndexQuery {
-    about: contentfulAboutContentRichTextNode {
-      childContentfulRichText {
-        html
+    about: contentfulAbout {
+      shields
+
+      childContentfulAboutContentRichTextNode {
+        childContentfulRichText {
+          html
+        }
       }
     }
   }
