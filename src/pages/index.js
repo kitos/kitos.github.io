df1ae3d5016031fcab6a4d258452feb40d2945e8
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-12-16 23:23:16 +0300

    feat: :sparkles: resolve #5 - list skills from projects and talks on about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,30 +4,60 @@ import { Box, Flex } from '@rebass/grid'
 
 import Layout from '../components/layout'
 import { Shield } from '../components/shield'
+import Badge from '../components/badge'
 
-const IndexPage = ({ data: { about } }) => (
-  <Layout>
-    <Flex mt="30px" flexWrap="wrap">
-      {about.shields.map(s => {
-        let [label, value] = s.split('|')
+const IndexPage = ({ data: { about, portfolio, talks } }) => {
+  let projectTechnologies = portfolio.edges.flatMap(
+    project => project.node.technologies
+  )
+  let talkTags = talks.edges.flatMap(project => project.node.tags)
 
-        return (
-          <Box mr="10px">
-            <Shield {...{ label, value }} />
+  let skillsMap = projectTechnologies
+    .concat(talkTags)
+    .reduce(
+      (map, skill) =>
+        map.has(skill) ? map.set(skill, map.get(skill) + 1) : map.set(skill, 1),
+      new Map()
+    )
+
+  let skills = Array.from(skillsMap.entries()).sort(
+    ([, count2], [, count1]) => count1 - count2
+  )
+
+  return (
+    <Layout>
+      <Flex my="30px" flexWrap="wrap">
+        {about.shields.map(s => {
+          let [label, value] = s.split('|')
+
+          return (
+            <Box mr="10px">
+              <Shield {...{ label, value }} />
+            </Box>
+          )
+        })}
+      </Flex>
+
+      <h2>Skills</h2>
+
+      <Flex flexWrap="wrap">
+        {skills.map(([t, count]) => (
+          <Box as={Badge} key={t} mr="5px" mb="5px">
+            {t}
           </Box>
-        )
-      })}
-    </Flex>
+        ))}
+      </Flex>
 
-    <div
-      dangerouslySetInnerHTML={{
-        __html:
-          about.childContentfulAboutContentRichTextNode.childContentfulRichText
-            .html,
-      }}
-    />
-  </Layout>
-)
+      <div
+        dangerouslySetInnerHTML={{
+          __html:
+            about.childContentfulAboutContentRichTextNode
+              .childContentfulRichText.html,
+        }}
+      />
+    </Layout>
+  )
+}
 
 export let query = graphql`
   query IndexQuery {
@@ -37,6 +67,22 @@ export let query = graphql`
       childContentfulAboutContentRichTextNode {
         childContentfulRichText {
           html
+        }
+      }
+    }
+
+    portfolio: allContentfulPortfolio {
+      edges {
+        node {
+          technologies
+        }
+      }
+    }
+
+    talks: allContentfulPublicActivity {
+      edges {
+        node {
+          tags
         }
       }
     }
