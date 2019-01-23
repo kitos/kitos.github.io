a66b603983eb9962bf6182118cb1600e252f3e18
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2019-01-23 18:14:21 +0300

    feat: :lipstick: animate skills

--- src/components/about/skills.js
+++ src/components/about/skills.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Box, Flex } from '@rebass/grid'
+import { animated, Trail } from 'react-spring'
 
 import { Badge } from '../badge'
 import { UnstyledButton } from '../button'
@@ -7,8 +8,10 @@ import { isMobile } from '../../utils'
 
 const MAX_SKILLS_TO_DISPLAY = isMobile ? 10 : 20
 
+let AnimatedBadge = animated(Badge)
+
 let SkillBadge = ({ children, ...rest }) => (
-  <Box as={Badge} mr="5px" mb="5px" {...rest}>
+  <Box as={AnimatedBadge} mr="5px" mb="5px" {...rest}>
     {children}
   </Box>
 )
@@ -17,17 +20,28 @@ let Skills = ({ skills }) => {
   let [expandSkills, toggleSkills] = useState(false)
 
   return (
-    <Flex flexWrap="wrap">
-      {(expandSkills ? skills : skills.slice(0, MAX_SKILLS_TO_DISPLAY)).map(
-        ({ name, mentions }) => (
-          <SkillBadge key={name}>
+    <Flex flexWrap="wrap" style={{ perspective: 500 }}>
+      <Trail
+        native
+        force
+        config={{
+          tension: 2000,
+          friction: 100,
+        }}
+        items={expandSkills ? skills : skills.slice(0, MAX_SKILLS_TO_DISPLAY)}
+        keys={item => item.name}
+        from={{ transform: 'rotateX(-90deg)', opacity: 0 }}
+        to={{ transform: 'rotateX(0)', opacity: 1 }}
+      >
+        {({ name, mentions }) => style => (
+          <SkillBadge style={style}>
             {name} <b>|</b>{' '}
             <span title="Mentioned in portfolio and public activities.">
               {mentions}
             </span>
           </SkillBadge>
-        )
-      )}
+        )}
+      </Trail>
 
       <UnstyledButton onClick={() => toggleSkills(!expandSkills)}>
         <SkillBadge
