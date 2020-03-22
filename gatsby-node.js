69461ca994a03daca08dad022dcec397a6a40937
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2020-03-22 10:19:07 +0100

    fix: :poop: :horse: remov netlify-identity-widget from site pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,15 @@
 const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+const webpack = require('webpack')
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^netlify-identity-widget$/,
+      }),
+    ],
+  })
+}
 
 exports.onCreateNode = ({ node }) => fmImagesToRelative(node)
 exports.sourceNodes = require('./src/gatsby/source-nodes')
